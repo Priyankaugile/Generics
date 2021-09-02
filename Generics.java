58ed0df757d4698198d487b37115c9baa4b2b821
priyankaugile <ugilepriyanka26@gmail.com>
2021-09-02 20:15:33 +0530

    Added: Code of maximum number with string.

--- Generics.java
+++ Generics.java
@@ -1,28 +1,32 @@
 package com.generics;
 
-public class MaximumNumberWithFloat {
-    //returns the maximum value of three floats
-    public static Float findMax(Float m, Float n, Float o) {
-        Float max = m;
-        if(n.compareTo(max)> 0)
-            max = n;
-        if(o.compareTo(max)>0)
-            max = o;
-        System.out.printf("The max of three numbers %s %s %s is : %s \n",m,n,o,max);
-        return max;
-    }
+import java.util.Scanner;
+
+public class MaximumNumberWithString {
+   //returns the max string name of three strings
+
+   public static String findMax(String m, String n, String o) {
+       String max = m;
+       if(n.compareTo(max)> 0)
+           max = n;
+       if(o.compareTo(max)>0)
+           max = o;
+       System.out.printf("The max of three numbers %s %s %s is : %s \n",m,n,o,max);
+       return max;
+   }
     public static void main(String[] args) {
-        //TC1.1 maximum number at first
-        Float float1 = 3.45f, float2 = 45.30f,float3 = 87.6f;
-        Float max = findMax(float1,float2,float3);
+        //TC1.1 maximum string name at first
+        String String1 = "Apple", String2 = "Google",String3 = "Microsoft";
+        String max1 = findMax(String1,String2,String3);
 
-        //TC1.2 maximum number at second
-        Float float4 = 23.0f,float5 = 98.98f,float6 = 4876.76f;
-        float max1 = findMax(float4,float5,float6);
+        //TC1.2 maximum string name  at second
+        String String4 = "iphone", String5 = "Amazon",String6 = "Royal";
+        String max2 = findMax(String4,String5,String6);
 
-        //TC1.3 maximum number at third
-        Float float7 = 36.56f,float8 = 73.65f,float9 = 42.06f;
-        float max2 = findMax(float7,float8,float9);
+        //TC1.3 maximum string name at third
+        String String7 = "Hathaway", String8 = "Dollar",String9 = "phone";
+        String max3 = findMax(String7,String8,String9);
     }
 }
+
 
